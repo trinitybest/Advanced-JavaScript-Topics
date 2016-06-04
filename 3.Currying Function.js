c68f9bf0880bd57108dd32cf25c4b813283e7862
TH <hutianyou6@gmail.com>
2016-06-04 19:26:06 +1200

    Update 3.Currying Function.js

--- 3.Currying Function.js
+++ 3.Currying Function.js
@@ -6,3 +6,33 @@ var add = function(a){
 }
 var addToFive = add(5); 
 console.log(addToFive(1));
+
+// Get the average of n arguments
+var avg = function(...n){//n arguments
+	let tot=0;
+  for(let i=0; i<n.length; i+=1){
+  	tot += n[i];
+  }
+  return tot/n.length;
+}
+// Calcualte the avg of n+m arguments
+var spiceUp = function(fn, ...n){
+	return function(...m){
+  	return fn.apply(this, n.concat(m));
+  }
+}
+
+var doAvg = spiceUp(avg, 1,2,3);
+//console.dir(doAvg);
+console.log(doAvg(4, 5, 6));
+
+var sayWhat = function(a){
+	return function(b){
+  	return function(c){
+    	console.log("saying " + a + " to " + b + " using " +c);
+    }
+  }
+};
+
+sayWhat('hi')('me')('nothing');
+
