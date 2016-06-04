ad7dd1603375f18c9ca15908a1f464096c7cb590
TH <hutianyou6@gmail.com>
2016-06-04 18:34:04 +1200

    Update 2.Callback Functions.js

--- 2.Callback Functions.js
+++ 2.Callback Functions.js
@@ -10,3 +10,27 @@ let y = function(callback){
 }
 
 y(x);
+
+let add = function(a, b){
+	return a+b;
+};
+let multiply = function(a, b){
+	return a*b;
+};
+let doWhatever = function(a, b){
+	console.log('here are your two numbers back',a,b);
+};
+
+let calc = function(num1, num2, callback){
+	if (typeof callback === "function"){
+  	return callback(num1, num2);
+  }
+	
+}
+
+console.log(calc(2,3, add));
+console.log(calc(2,3, multiply));
+console.log(calc(2,3, doWhatever));
+console.log(calc(2,3, function(a, b) {
+	return a - b;
+}));
