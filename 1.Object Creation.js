94654124bd78f87100633c0815cd33147f50ff09
TH <hutianyou6@gmail.com>
2016-06-04 17:40:03 +1200

    Create 1.Object Creation.js

---
+++ 1.Object Creation.js
@@ -0,0 +1,23 @@
+var peopleFactory = function(name, age, state){
+	var temp = {};
+  
+  temp.age = age;
+  temp.name = name;
+  temp.state = state;
+  
+  temp.printPerson = function(){
+  	console.log(this.name + ", " + this.age + ", " + this.state);
+  }
+  
+  return temp;
+};
+
+var person1 = peopleFactory('John', 23, 'CA');
+var person2 = peopleFactory('Kim', 27, 'SC');
+//document.write(person1);
+person1.printPerson();
+person2.printPerson();
+
+
+
+
