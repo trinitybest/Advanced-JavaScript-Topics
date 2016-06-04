668cfd7af42552453ea567f167aa57d4e8c91282
TH <hutianyou6@gmail.com>
2016-06-04 18:15:00 +1200

    Update 1.Object Creation.js

--- 1.Object Creation.js
+++ 1.Object Creation.js
@@ -19,6 +19,44 @@ person1.printPerson();
 person2.printPerson();
 
 // using prototype
+var peopleProto = function(){
+
+};
+//set default values
+peopleProto.prototype.name = "no name";
+peopleProto.prototype.age = 0;
+peopleProto.prototype.city = "no city";
+
+peopleProto.prototype.printPerson = function(){
+	console.log(this.name + ", " + this.age + ", " + this.state);
+}
+
+var person1 = new peopleProto();
+person1.name = 'John';
+person1.age = 23;
+person1.city = 'CA';
+
+person1.printPerson();
+//this will work if name property is in prototype or is in person1
+console.log('name' in person1);
+//this will only work if name property is in person1
+console.log(person1.hasOwnProperty('name'));
+//console.dir(peopleProto);
+
+//dynamic prototype
+var peopleDynamicProto = function(name, age, state){
+	this.age = age;
+  this.name = name;
+  this.state = state;
+  if(typeof this.printPerson !== 'function') {
+  	peopleDynamicProto.prototype.printPerson = function(){
+		console.log(this.name + ", " + this.age + ", " + this.state);
+		}
+  }
+};
+var person1 = new peopleDynamicProto('John', 23, 'CA');
+console.log('name' in person1);
+console.log(person1.hasOwnProperty('name'));
 
 
 
