d25706a9daa7aaf5531c038f0b40ad0ec7216b6b
TH <hutianyou6@gmail.com>
2016-06-05 12:23:28 +1200

    Update 2.call apply and bind.js

--- 2.call apply and bind.js
+++ 2.call apply and bind.js
@@ -6,3 +6,12 @@ var addToThis = function(a, b, c){
 var arr = [1,2,3];//could be used on apply, but could not be used for bind.
 var bound = addToThis.bind(obj);
 console.log(bound(1, 2, 3));
+//pseudo code
+var functionName = function(arg1, arg2, arg3){
+
+};
+functionName.call(obj, arg1, arg2, arg3);
+functionName.apply(obj, [arg1, arg2, arg3]);
+var bound = functionName.bind(obj);
+bound(arg1, arg2, arg3);
+
